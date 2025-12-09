1a53ba7c7d3bc0718cea36b4fc66756cf4e152fb
Giovanni Elias <giovanni.elias@flowe.com>
2025-12-10 00:34:44 +0100

    add missing trailing slashes in urls

--- backend/apps/automations/urls.py
+++ backend/apps/automations/urls.py
@@ -3,5 +3,5 @@ from .views import AutomationCreateAPIView, AutomationReadAPIView
 
 urlpatterns = [
     path("automations/", AutomationReadAPIView.as_view(), name="automation-read"),
-    path("automations/create", AutomationCreateAPIView.as_view(), name="automation-create"),
+    path("automations/create/", AutomationCreateAPIView.as_view(), name="automation-create"),
 ]
